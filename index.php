113abc004321d735f1e15713fe0db29925d8357b
felipe-rodrigues <felipe_rodrigues-silva@outlook.com>
2015-08-25 21:04:12 -0300

    Criação e instalação de bootstrap
    Criação dos arquivos para teste

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$codigo= $_POST[codigo];
+
+$palavras_reservadas =array('program','var','int','if','else','while','float','string','end' );
+$operadores = array('+','-','&&','|','==','<','>','<=','>=','=');
+$table_simbols= array();
+
+
+
+
+
+
